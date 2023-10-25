c93fa256ec5f7e39de1a35d5918a0a4dd6b7ab8e
Smit Modi <smitmodi21@gnu.ac.in>
2023-10-25 21:21:19 +0530

    A2

--- app/src/main/java/com/example/project_test_2/MainActivity.kt
+++ app/src/main/java/com/example/project_test_2/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.example.project_test_2
 
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var coinanimation: AnimationDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val coin: ImageView = findViewById(R.id.coin)
+        coin.setBackgroundResource(R.drawable.coin_animation)
+        coinanimation = coin.background as AnimationDrawable
 
         val btnTotalExpenditure = findViewById<Button>(R.id.btnTotalExpenditure)
         val btnAddExpense = findViewById<Button>(R.id.btnAddExpense)
@@ -23,6 +31,15 @@ class MainActivity : AppCompatActivity() {
             // Navigate to AddExpenseActivity
             val intent = Intent(this, AddExpenseActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            coinanimation.start()
+        } else {
+            coinanimation.stop()
         }
     }
 }
